4f033ac3d5b56336e7ad22c22d671479408ade41
Raja <rajakalavala55@gmail.com>
2022-10-29 22:56:39 +0530

    Projects Section - Create and complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import About from "../components/About";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Projects from "../components/Projects";
 import Skills from "../components/Skills";
 import WorkExperience from "../components/WorkExperience";
 import styles from "../styles/Home.module.css";
@@ -38,6 +39,9 @@ export default function Home() {
       </section>
 
       {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contat Me */}
     </div>
