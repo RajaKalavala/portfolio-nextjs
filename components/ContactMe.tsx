afc3f6450d9c20e7e29a8d6093ddd8e99b4117a6
Raja <rajakalavala55@gmail.com>
2022-10-30 11:04:27 +0530

    Contact Section - UI complete

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 
 type Props = {};
 
@@ -15,7 +16,49 @@ export default function ContactMe({}: Props) {
           <span className="decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
         </h4>
 
-        <div></div>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 ">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">+919668501411</p>
+          </div>
+
+          <div className="flex items-center space-x-5 ">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">rajakalavala55@gmail.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 ">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-7 animate-pulse " />
+            <p className="text-2xl">Bengaluru</p>
+          </div>
+        </div>
+
+        <form className="flex flex-col space-y-2 w-fit mx-auto">
+          <div className="flex space-x-2">
+            <input
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            ></input>
+            <input
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            ></input>
+          </div>
+          <input
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          ></input>
+          <textarea placeholder="Message" className="contactInput"></textarea>
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
